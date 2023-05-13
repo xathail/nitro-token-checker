984dbd7c81a7598c31cc4b66d934b976665fa17d
xzerios <108062540+xzerios@users.noreply.github.com>
2023-05-13 23:30:09 +0000

    Complete Recode!

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Nitro Token Checker
-- I made this on mobile so honestly I don't care about the quality of the code
-- People actually sell these when it's just one request to see if an account has boosts D:
+## This Has Been Officially Tested Using [MyProxies](http://myproxies.io) 
+> + If you need help at any point, please contact me on Discord!
+> + If this repository helped you, please leave a star! It shows me that people DO actually use these tools.
 
 ## How To Use
 > 1) Clone / download this repository 
 > 2) Navigate into the file
 > 3) Put tokens into tokens.txt
 > 4) Run `pip install -r requirements.txt` 
+> 5) Insert proxy where it says "ENTER PROXY HERE" - Checkout [MyProxies](https://myproxies.io)
 > 5) Run `python main.py`
-> 6) Tokens will now be checked & written to valids.txt
+> 6) Tokens will now be checked & written nicely into a folder
+
+## Remove Proxy Support
+> + Option 1) DM me on Discord and I can help [£xerius#0975](http://discord.gg/funnysite)
+> + Option 2) Remove all mentions of 'proxy' / 'proxies' in the code.
--- main.py
+++ main.py
@@ -1,46 +1,75 @@
-import concurrent.futures, requests, datetime;from colorama import Fore, Back, Style
-
-def check_token(token):
-    availableBoosts = 0
-    now = datetime.datetime.now(datetime.timezone.utc)
-    response = requests.get("https://discord.com/api/v9/users/@me", headers={"Authorization": token}) 
-    if response.status_code == 200: 
-        user = response.json()
-        if user["premium_type"] != 0: 
-            response = requests.get("https://discord.com/api/v9/users/@me/guilds/premium/subscription-slots", headers={"Authorization": token}) # check available boosts
-            boostCheck = response.json()
-            for nitroToken in boostCheck:
-                if 'cooldown_ends_at' in nitroToken:
-                    if nitroToken['cooldown_ends_at'] == None: 
-                        availableBoosts += 1
-                        continue
-                    else: 
-                        cooldownEndStr = nitroToken['cooldown_ends_at'].split('+')[0] + '+00:00' 
-                        cooldownEnds = datetime.datetime.fromisoformat(cooldownEndStr)
-                        if cooldownEnds <= now:  
-                            availableBoosts += 1        
-        print(Fore.GREEN + token)
-        f=open("valids.txt", "a")
-        f.write(f"{token} | Boosts: {availableBoosts} = {user['username']}#{user['discriminator']}\n")
-    else: 
-        print(Fore.RED + token)
-        f=open("invalids.txt", "a")
-        f.write(f"{token}\n")
-    return
+import concurrent.futures, requests, datetime, os
+proxy = {'http': 'ENTER PROXY HERE','https': 'ENTER PROXY HERE'}
 
 def main():
-    tokens = open("tokens.txt", "r").readlines() 
-    tokens = [token.rstrip() for token in tokens] 
+    tokens = open("tokens.txt", "r").readlines()
+    tokens = [token.rstrip() for token in tokens]
     num_threads = int(input("Enter the number of threads to use: "))
-
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         futures = []
+        start = datetime.datetime.now()
+        session = requests.Session()
         for token in tokens:
-            futures.append(executor.submit(check_token, token))
-
+            futures.append(executor.submit(updatedChecker, session, token))
         for future in concurrent.futures.as_completed(futures):
             future.result()
+        session.close()
+    end = datetime.datetime.now()
+    print("Successfully checked",len(tokens),"tokens in", end-start)
 
-    print("Successfully checked",len(tokens),"tokens")
+def updatedChecker(session, token):
+    now = datetime.datetime.now(datetime.timezone.utc)
+    directory = now.strftime("%d-%m-%Y %H:%M")
+    os.makedirs(directory, exist_ok=True)
+    headers = {"Authorization": token}
+    try:
+        response = session.get("https://discord.com/api/v9/users/@me", headers=headers, proxies=proxy)
+        response.raise_for_status() 
+        user = response.json()
+    except (requests.exceptions.HTTPError, ValueError):
+        print(f"\033[31m{token}")
+        with open(f"{directory}/Invalids.txt", "a") as f:
+            f.write(f"{token}\n")
+        return
+    
+    if user["premium_type"] == 0:
+        print(f"\033[32m{token}")
+        with open(f"{directory}/NoNitro.txt", "a") as f:
+            f.write(f"{token} = {user['username']}#{user['discriminator']}\n")
+    elif user["premium_type"] == 2:
+        try:
+            response = session.get("https://discord.com/api/v9/users/@me/guilds/premium/subscription-slots", headers=headers, proxies=proxy)
+            response.raise_for_status()
+            boost_check = response.json()
+        except requests.exceptions.HTTPError:
+            boost_check = []
+        available_boosts = 0
+        for nitro_token in boost_check:
+            cooldown_ends_at = nitro_token.get('cooldown_ends_at')
+            if cooldown_ends_at is None or datetime.datetime.fromisoformat(cooldown_ends_at.split('+')[0] + '+00:00').replace(tzinfo=datetime.timezone.utc) <= now:
+                available_boosts += 1
+        nitro_expires = "Never"
+        p = '| Server Boosted: None '
+        if boost_check and boost_check[0]['premium_guild_subscription'] is not None:
+            p = f"| Server Boosted: {boost_check[0]['premium_guild_subscription']['guild_id']} "
+            try:
+                response = session.get("https://discord.com/api/v9/users/@me/billing/subscriptions", headers=headers, proxies=proxy)
+                response.raise_for_status()
+                nitro_check = response.json()
+                nitro_expires = datetime.datetime.fromisoformat(nitro_check[0]['trial_ends_at']).replace(tzinfo=datetime.timezone.utc).strftime('%d-%m-%Y %H:%M')
+            except requests.exceptions.HTTPError:
+                pass
+        try:    
+            print(f"\033[32m{token}")
+            with open(f"{directory}/BoostTokens.txt", "a") as f:
+                f.write(f"{token} | Available Boosts: {available_boosts} | Expires: {nitro_expires} {p} | {user['username']}#{user['discriminator']}\n")
+        except:
+            print(f"\033[32m{token}")
+            with open(f"{directory}/BoostTokens.txt", "a") as f:
+                f.write(f"{token} | Boosts: {available_boosts} | Expires: Never {p} | {user['username']}#{user['discriminator']}\n")
+    else:
+        print(f"\033[32m{token}")
+        with open(f"{directory}/NitroTokens.txt", "a") as f:
+            f.write(f"{token} | {user['username']}#{user['discriminator']}\n")
 
 main()
